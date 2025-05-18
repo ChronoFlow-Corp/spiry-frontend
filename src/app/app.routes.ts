8d38cb234d6ec9019906667e6617e7383c77fba4
nnabse <nnabse@gmail.com>
2025-05-18 21:20:02 +0700

    [setup] add wildcard redirects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'web', pathMatch: 'full'},
   {
-    path: '',
+    path: 'web',
     loadComponent: () =>
       import('@pages/web/web.component').then((c) => c.WebComponent),
   },
+  {path: '**', redirectTo: 'web'},
 ];
