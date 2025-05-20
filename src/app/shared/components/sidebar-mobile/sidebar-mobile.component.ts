4c82c58d0119a8b603673132906b2869c4d9d525
nnabse <nnabse@gmail.com>
2025-05-20 11:40:00 +0700

    [sidebar-mobile] add debug logs

--- src/app/shared/components/sidebar-mobile/sidebar-mobile.component.ts
+++ src/app/shared/components/sidebar-mobile/sidebar-mobile.component.ts
@@ -1,7 +1,14 @@
-import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 import {SidebarMobileHeaderComponent} from '@components/sidebar-mobile/header/sidebar-mobile-header.component';
 import {SidebarMobileMenuComponent} from '@components/sidebar-mobile/menu/sidebar-mobile-menu.component';
+import {DebuggerService} from '@service/debugger/debugger.service';
 import {SidebarMobileStore} from '@store/sidebar-mobile/sidebar-mobile.store';
 
 @Component({
@@ -11,7 +18,17 @@ import {SidebarMobileStore} from '@store/sidebar-mobile/sidebar-mobile.store';
   styleUrl: './sidebar-mobile.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarMobileComponent {
+export class SidebarMobileComponent implements OnInit, OnDestroy {
   protected readonly $isMenuShown =
     inject(SidebarMobileStore).state.isMenuShown;
+
+  readonly #debuggerService = inject(DebuggerService);
+
+  ngOnInit(): void {
+    this.#debuggerService.log('SidebarMobileComponent', 'init');
+  }
+
+  ngOnDestroy(): void {
+    this.#debuggerService.log('SidebarMobileComponent', 'destroy');
+  }
 }
