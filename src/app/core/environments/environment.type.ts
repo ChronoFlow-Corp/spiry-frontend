788eb3586b95fa3868d6cd5d1f4c1f8072c6b834
nnabse <nnabse@gmail.com>
2025-05-20 11:39:36 +0700

    [sidebar-mobile] add debug logs

--- src/app/core/environments/environment.type.ts
+++ src/app/core/environments/environment.type.ts
@@ -4,6 +4,7 @@ export type EnvironmentType = {
   origin: string;
   apiUrl: string;
   wsUrl: string;
+  debug: boolean;
 };
 
 export const ENVIRONMENT_TOKEN = new InjectionToken<EnvironmentType>(
--- src/app/core/services/sidebar-switcher/sidebar-switcher.service.spec.ts
+++ src/app/core/services/sidebar-switcher/sidebar-switcher.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { SidebarSwitcherService } from './sidebar-switcher.service';
+import {SidebarSwitcherService} from './sidebar-switcher.service';
 
 describe('SidebarSwitcherService', () => {
   let service: SidebarSwitcherService;
