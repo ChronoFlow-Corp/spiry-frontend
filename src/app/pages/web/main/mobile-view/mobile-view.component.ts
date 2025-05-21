d2fb2ec1e72d412fa1406a7b24305cfb4c24924c
nnabse <nnabse@gmail.com>
2025-05-21 21:47:45 +0700

    [page-web] add ai input component

--- src/app/pages/web/main/mobile-view/mobile-view.component.ts
+++ src/app/pages/web/main/mobile-view/mobile-view.component.ts
@@ -1,7 +1,10 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
 
+import {AiInputComponent} from '@shared/components/ai-input/ai-input.component';
+
 @Component({
   selector: '.page-web-main-mobile-view',
+  imports: [AiInputComponent],
   templateUrl: './mobile-view.component.html',
   styleUrl: './mobile-view.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
